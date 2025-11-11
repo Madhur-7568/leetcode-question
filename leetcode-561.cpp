4748392b1beddc1ed500c5407871274841a48cf7
madhur dhillon <madhurdhillon7@gmail.com>
2025-11-12 00:01:05 +0530

    Array Partition

---
+++ leetcode-561.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+      int n=nums.size();
+   // int n=4;
+        sort(nums.begin(),nums.end());
+        int max=0;
+        for(int i=0;i<n;i=i+2){
+                    max=min(nums[i],nums[i+1])+max;
+        }
+             return max;
+    }
+    int main(){
+        vector<int>nums={1,4,3,2};
+        arrayPairSum(nums);
+        return 0;
+    }
+};

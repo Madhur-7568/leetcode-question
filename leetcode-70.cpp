95c9bb35878c1faf0daaa2feca27d041c74fdf2e
madhur dhillon <madhurdhillon7@gmail.com>
2025-11-14 22:45:05 +0530

    Climbing Stairs

---
+++ leetcode-70.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int a=1;
+        int b=2;
+        int c=0;
+        for(int i=3;i<=n;i++){
+         c=a+b;
+        a=b;
+        b=c;
+        }
+        return c;
+    }
+    int main(){
+        climbStairs(4);
+        return 0;
+    }
+};
